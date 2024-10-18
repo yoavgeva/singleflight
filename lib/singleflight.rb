4b2f0c7e6c0b93186b4f9a7186aa7c5b28f53d97
ygeva <Yoav.Geva@cyberark.com>
2024-10-18 21:37:33 +0300

    first draft

---
+++ lib/singleflight.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "singleflight/version"
+
+module Singleflight
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/singleflight/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module Singleflight
+  VERSION = "0.1.0"
+end
